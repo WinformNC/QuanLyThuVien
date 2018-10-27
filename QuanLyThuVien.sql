588be334d11e6024f0d82ccb7d8f775d4c95190f
TranDuc3997 <vanduc3997@gmail.com>
2018-10-27 08:18:12 +0700

    AAA

--- QuanLyThuVien.sql
+++ QuanLyThuVien.sql
@@ -1,10 +1,10 @@
-/*==============================================================*/
+﻿/*==============================================================*/
 /* DBMS name:      Microsoft SQL Server 2012                    */
 /* Created on:     10/22/2018 8:01:46 PM                        */
 /*==============================================================*/
 
 
-
+create database QuanLyThuVien
 if exists (select 1
             from  sysobjects
            where  id = object_id('BIENBANVIPHAM')
@@ -257,7 +257,7 @@ create table SACH (
    NAMXB                int                  null,
    HINHANHSACH          nvarchar(100)        null,
    MOTA                 nvarchar(1500)       null,
-
+   SOLUONG				int					null,
    constraint PK_SACH primary key (MASACH),
    constraint FK_SACH_REFERENCE_NHAXB foreign key (MANXB)
       references NHAXB (MANXB),
@@ -294,5 +294,26 @@ create table PHAT
 	constraint FK_PHAT_REFERENCE_SINHVIEN foreign key (MASINHVIEN)
 	  references SINHVIEN(MASINHVIEN)
 )
+insert into THELOAI
+values('TL001',N'Công Nghệ'),
+('TL002',N'Khoa Học'),
+('TL003',N'Thực Phẩm')
 
+insert into NHAXB
+values('XB001',N'NXB Trẻ','11111111','090009900'),
+('XB002',N'NXB Thanh Niên','112111','090033900'),
+('XB003','NXB Vị Thành Niên','111211','09012100')
+insert into TACGIA
+values('TG001',N'Trần Lê Văn Đức','0900552312','1212121'),
+('TG002',N'Nguyễn Thái Bảo','0900532312','12312121'),
+('TG003',N'Nguyễn Thanh Quang','09252312','121312321121')
+insert into VITRI
+values('VT001','A1'),
+('VT002','A2'),
+('VT003','A3')
 
+CREATE VIEW sachView
+as select s.*,TENTACGIA,TENNXB,TENTHELOAI
+from Sach s, TACGIA tg, VITRI vt, NHAXB nxb, THELOAI tl
+where s.MANXB = nxb.MANXB and s.MATACGIA = tg.MATACGIA and s.MATHELOAI = tl.MATHELOAI and s.MAVITRI = vt.MAVITRI;
+select *  from sachView
---
+++ importData.sql
@@ -0,0 +1,16 @@
+insert into THELOAI
+values('TL001','Công Nghệ'),
+('TL002','Khoa Học'),
+('TL003','Thực Phẩm')
+insert into NHAXB
+values('XB001','NXB Trẻ','11111111','090009900'),
+('XB002','NXB Thanh Niên','112111','090033900'),
+('XB003','NXB Vị Thành Niên','111211','09012100')
+insert into TACGIA
+values('TG001','Trần Lê Văn Đức','0900552312','1212121'),
+('TG002','Nguyễn Thái Bảo','0900532312','12312121'),
+('TG003','Nguyễn Thanh Quang','09252312','121312321121')
+insert into VITRI
+values('VT001','A1'),
+('VT002','A2'),
+('VT003','A3')
